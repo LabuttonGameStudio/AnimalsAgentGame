35c5b1f346ea4d2fd115bc7baa8fef8cb81133db
CharmingParrot <contato.pedro2761@gmail.com>
2024-03-11 15:41:52 -0300

    Implementação TP teste

--- Assets/Prototipagem/Pedro/Testes/Teleporte.cs
+++ Assets/Prototipagem/Pedro/Testes/Teleporte.cs
@@ -4,10 +4,18 @@ using UnityEngine;
 
 public class Teleporte : MonoBehaviour
 {
-    public Transform Destination;
-    public Transform player;
-    public void OnTriggerEnter(Collider other)
+    public Transform startPoint; // Ponto de partida
+    public Transform teleportPoint; // Ponto para teletransporte
+
+    private void OnTriggerEnter(Collider other)
     {
-        player.transform.position = Destination.transform.position;
+        if (other.CompareTag("Player"))
+        {
+            Debug.Log("Jogador entrou no trigger em: " + startPoint.position);
+            Debug.Log("Teleportando para: " + teleportPoint.position);
+
+            // Teleporta o jogador para o ponto de teletransporte
+            other.transform.position = teleportPoint.position;
+        }
     }
 }
