00a3e301812d74ff7475f8be9dc6945c9e89bb3b
CharmingParrot <88729082+CharmingParrot@users.noreply.github.com>
2024-04-08 12:39:11 -0300

    TutorialFase_v1.5
    Ajustes rapidos

--- Assets/Prototipagem/Pedro/Testes/Teleporte.cs
+++ Assets/Prototipagem/Pedro/Testes/Teleporte.cs
@@ -6,16 +6,25 @@ public class Teleporte : MonoBehaviour
 {
     public Transform startPoint; // Ponto de partida
     public Transform teleportPoint; // Ponto para teletransporte
+    public float teleportDelay = 1.0f; // Atraso antes do teletransporte
 
     private void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag("Player"))
         {
-            Debug.Log("Jogador entrou no trigger em: " + startPoint.position);
-            Debug.Log("Teleportando para: " + teleportPoint.position);
-
-            // Teleporta o jogador para o ponto de teletransporte
-            other.transform.position = teleportPoint.position;
+            StartCoroutine(TeleportAfterDelay(other.transform));
         }
+    }
+
+    private IEnumerator TeleportAfterDelay(Transform playerTransform)
+    {
+        Debug.Log("Jogador entrou no trigger em: " + startPoint.position);
+        Debug.Log("Teleportando para: " + teleportPoint.position);
+
+        // Aguarda o atraso antes de teletransportar o jogador
+        yield return new WaitForSeconds(teleportDelay);
+
+        // Teleporta o jogador para o ponto de teletransporte
+        playerTransform.position = teleportPoint.position;
     }
 }
