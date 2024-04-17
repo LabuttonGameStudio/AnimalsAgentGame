8645784b9be46b43ac6df4f8b72041e77db9c979
Rodrigo Morizono <rkmorizono@gmail.com>
2024-04-17 10:58:20 -0300

    Sprint Camera Pivot Offset(0.1v)

--- Assets/Prototipagem/Mori/FirstGameplayTest/Script/FPCameraShake.cs
+++ Assets/Prototipagem/Mori/FirstGameplayTest/Script/FPCameraShake.cs
@@ -10,6 +10,15 @@ public class ShakeStats
     public float amplitudeGain;
     public float frequency;
 }
+public class PivotOffsetStats
+{
+    public Coroutine coroutine;
+    public Vector3 currentOffset;
+    public Vector3 minPivotOffset;
+    public Vector3 maxPivotOffset;
+    public float frequency;
+    public float duration;
+}
 public class FPCameraShake : MonoBehaviour
 {
     private static FPCameraShake Instance;
@@ -22,6 +31,7 @@ public class FPCameraShake : MonoBehaviour
         channelPerlin = virtualCamera.GetCinemachineComponent<CinemachineBasicMultiChannelPerlin>();
         Instance = this;
     }
+    #region Shake
     public static ShakeStats StartShake(float duration, float amplitudeGain, float frequency)
     {
         ShakeStats stats = new ShakeStats() { duration = duration, amplitudeGain = amplitudeGain, frequency = frequency };
@@ -53,4 +63,36 @@ public class FPCameraShake : MonoBehaviour
         }
 
     }
+    #endregion
+
+    #region PivotOffSet
+
+    public static PivotOffsetStats StartPivotOffset(float duration, float frequency, Vector3 minOffset,Vector3 maxOffset)
+    {
+        PivotOffsetStats stats = new PivotOffsetStats() { duration = duration, minPivotOffset = minOffset, maxPivotOffset = maxOffset,frequency = frequency };
+        stats.coroutine = Instance.StartCoroutine(Instance.PivotOffset_Coroutine(stats, true));
+        return stats;
+    }
+    public static PivotOffsetStats StartPivotOffset(float frequency, Vector3 minOffset, Vector3 maxOffset)
+    {
+        PivotOffsetStats stats = new PivotOffsetStats() {minPivotOffset = minOffset, maxPivotOffset = maxOffset, frequency = frequency };
+        stats.coroutine = Instance.StartCoroutine(Instance.PivotOffset_Coroutine(stats, false));
+        return stats;
+    }
+    public static void StopPivotOffset(PivotOffsetStats stats)
+    {
+        
+    }
+    private IEnumerator PivotOffset_Coroutine(PivotOffsetStats stats,bool hasDuration)
+    {
+        float timer = 0;
+
+        while (true)
+        {
+            stats.currentOffset = Vector3.Lerp(Vector3.up, Vector3.up * (-1), Mathf.Sin(Time.time)) ;
+            yield return null;
+        }
+
+    }
+    #endregion
 }
