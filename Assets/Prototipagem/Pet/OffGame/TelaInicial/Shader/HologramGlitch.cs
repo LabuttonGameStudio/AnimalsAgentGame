4128cc8a3395be45c0fb38e52542c2d29cb533e3
Petkovic Dos Santos <89022214+Petinho29@users.noreply.github.com>
2024-06-01 22:52:36 -0300

    UI - Icon enemy v.0.2 + Tela Pause v.0.0

--- Assets/Prototipagem/Pet/OffGame/TelaInicial/Shader/HologramGlitch.cs
+++ Assets/Prototipagem/Pet/OffGame/TelaInicial/Shader/HologramGlitch.cs
@@ -4,16 +4,22 @@ using UnityEngine;
 
 public class HologramGlitch : MonoBehaviour
 {
+    [Header("GLITCH")]
     public float maxGlitch;
-
     public float minGlitch;
 
     public float glitchLength;
-
     public float timeBetweenGlitches;
+
+    [Header("SHINE")]
+    public float maxShine;
+    public float minShine;
+
+    public float timeBetweenShine;
 
     private void Start()
     {
+        StartCoroutine(Shine());
         StartCoroutine(Glitch());
     }
 
@@ -32,6 +38,39 @@ public class HologramGlitch : MonoBehaviour
         mainMaterial.SetFloat("_Glitch_Strength", 0f);
 
         StartCoroutine(Glitch());
+    }
+
+    IEnumerator Shine()
+    {
+        Renderer renderer = GetComponent<Renderer>();
+
+        Material mainMaterial = renderer.material;
+
+        while (true)
+        {
+            //aumenta
+            yield return ChangeShine(mainMaterial, minShine, maxShine, timeBetweenShine);
+
+            // diminui
+            yield return ChangeShine(mainMaterial, maxShine, minShine, timeBetweenShine);
+        }
+
+    }
+
+    IEnumerator ChangeShine(Material material, float startShine, float endShine, float duration)
+    {
+        float elapsedTime = 0f;
+        Color baseColor = material.GetColor("_Color") / Mathf.Max(material.GetColor("_Color").maxColorComponent, 1); // normalize
+
+        while (elapsedTime < duration)
+        {
+            elapsedTime += Time.deltaTime;
+            float intensity = Mathf.Lerp(startShine, endShine, elapsedTime / duration);
+            material.SetColor("_Color", baseColor * intensity);
+            yield return null;
+        }
+
+        material.SetColor("_Color", baseColor * endShine); // modificar o intensity do hdr apenas
     }
 
 }
