a3da05874f4e86aab617fd5e6b09c34c63c071f9
Mori <contato.devmori@gmail.com>
2024-10-08 08:03:07 -0300

    DoorWall Interactive

---
+++ Assets/Prototipagem/Mori/FirstGameplayTest/Script/DoorWallClosed_Int.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class DoorWallClosed_Int : MonoBehaviour,InteractiveObject
+{
+    [SerializeField]private DoorWallOpen_Int openDoor;
+
+    public string GetObjectName()
+    {
+        return "Portão";
+    }
+    public string GetObjectDescription()
+    {
+        return "Abrir";
+    }
+    public void Interact(InputAction.CallbackContext value)
+    {
+        gameObject.SetActive(false);
+        openDoor.gameObject.SetActive(true);
+    }
+
+}
---
+++ Assets/Prototipagem/Mori/FirstGameplayTest/Script/DoorWallOpen_Int.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class DoorWallOpen_Int : MonoBehaviour, InteractiveObject
+{
+    [SerializeField] private DoorWallClosed_Int closedDoor;
+
+    public string GetObjectName()
+    {
+        return "Portão";
+    }
+    public string GetObjectDescription()
+    {
+        return "Fechar";
+    }
+    public void Interact(InputAction.CallbackContext value)
+    {
+        gameObject.SetActive(false);
+        closedDoor.gameObject.SetActive(true);
+    }
+}
