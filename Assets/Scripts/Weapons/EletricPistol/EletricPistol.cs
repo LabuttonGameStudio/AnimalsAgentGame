924cc63d26e21ce087e0f8a85ad43ca97e3a0222
Mori <contato.devmori@gmail.com>
2024-10-18 09:58:49 -0300

    Electric Pistol (FPS Update 0.1v)

--- Assets/Scripts/Weapons/EletricPistol/EletricPistol.cs
+++ Assets/Scripts/Weapons/EletricPistol/EletricPistol.cs
@@ -12,14 +12,16 @@ public class EletricPistol : Weapon
     public EletricPistol(ArmadilloWeaponControl armadilloWeaponControl)
     {
         weaponControl = armadilloWeaponControl;
-        ammoType = AmmoType.Overheat;
-        maxAmmoAmount = 100;
+        ammoType = AmmoType.Ammo;
+        maxAmmoAmount = 12;
+        currentAmmoAmount = 12;
+        maxAmmoAmount = 24;
+        maxAmmoReserveAmount = 36;
         name = "Pistola Eletrica";
         description = "Pressione pra atirar, segure para carregar ";
         ammoSlider = armadilloWeaponControl.Weaponammoslider;
         weaponType = WeaponType.Zapgun;
         LoadVisual();
-        UpdateOverheatHUD();
     }
 
     //----- Stats -----
@@ -28,17 +30,9 @@ public class EletricPistol : Weapon
     private bool isOnCooldown;
 
     readonly private int unchargedHitDamage = 15;
-    readonly private int unchargedOverheatCharge = 25;
-    //Tiro carregado
-    readonly private int chargedHitDamage = 50;
-    readonly private float chargeTime = 0.25f;
-    readonly private int chargedOverheatCharge = 100;
-    //Overheat
-    private bool isOverheating;
-    private Image AmmoImage;
-    readonly private float overheatDuration = 1.5f;
-    readonly private float timeUntilPassiveCoolOffStarts = 1f;
-    readonly private float timeToFullCoolOff = 1.5f;
+
+    readonly private float damageFallOffStartRange;
+    readonly private float damageFallOffEndRange;
     //----- Visual -----
     private EletricPistolVisual visualHandler;
 
@@ -46,22 +40,6 @@ public class EletricPistol : Weapon
     #region
     private void LoadVisual()
     {
-        /*
-        GameObject modelPrefab;
-        modelPrefab = Resources.Load<GameObject>("Prefabs/Weapons/EletricPistol");
-        if (modelPrefab == null)
-        {
-            Debug.LogError("Erro ao encontrar Eletric Pistol Prefab em Prefabs/Weapons/EletricPistol");
-        }
-        GameObject model;
-        model = weaponControl.LoadModel(modelPrefab, weaponControl.eletricPistolSpawnPoint);
-        if (model.TryGetComponent(out EletricPistolVisual eletricPistolVisual))
-        {
-            visualHandler = eletricPistolVisual;
-            visualHandler.ToggleVisual(false);
-        }
-        else Debug.LogError("Erro ao encontrar visual handler EletricPistolVisual no prefab");
-        */
         visualHandler = EletricPistolVisual.Instance;
         visualHandler.ToggleVisual(false);
     }
@@ -111,55 +89,27 @@ public class EletricPistol : Weapon
 
     public override void OnFireButtonPerformed(InputAction.CallbackContext performed)
     {
-        if (!IsFireOnCooldown()) StartHoldOrPressTimer();
+        if (!IsFireOnCooldown()) Fire();
     }
     public override void OnFireButtonCanceled(InputAction.CallbackContext performed)
     {
-        if (!IsFireOnCooldown()) StopHoldOrPressTimer();
+        if (!IsFireOnCooldown()) Fire();
     }
 
-    public void StartHoldOrPressTimer()
+    public void Fire()
     {
-        if (!isOverheating)
-        {
-            if (holdOrPressTimerRef == null) holdOrPressTimerRef = weaponControl.StartCoroutine(HoldOrPressTimer_Coroutine());
-            else
-            {
-                StopHoldOrPressTimer();
-                holdOrPressTimerRef = weaponControl.StartCoroutine(HoldOrPressTimer_Coroutine());
-            }
-        }
-    }
-    public void StopHoldOrPressTimer()
-    {
-        if (holdOrPressTimerRef != null)
-        {
-            weaponControl.StopCoroutine(holdOrPressTimerRef);
-            holdOrPressTimerRef = null;
-            Fire(holdOrPressTimer);
-        }
-    }
-    public void Fire(float timer)
-    {
-        if (timer < chargeTime)
-        {
-            FPCameraShake.StartShake(0.15f, 0.4f, 5f);
-            UnChargedFire();
-        }
-        else
-        {
-            FPCameraShake.StartShake(0.4f, 0.8f, 6f);
-            ChargedFire();
-        }
+        FPCameraShake.StartShake(0.15f, 0.4f, 5f);
+        FireRaycast();
         ArmadilloPlayerController.Instance.visualControl.OnEletricGunFire();
         ArmadilloPlayerController.Instance.visualControl.ToggleEletricPistolCharge(false);
         StartFireCooldownTimer();
     }
-    public void UnChargedFire()
+    public void FireRaycast()
     {
         RaycastHit[] targetsHit;
         Transform camera = ArmadilloPlayerController.Instance.cameraControl.mainCamera.transform;
         targetsHit = Physics.RaycastAll(camera.position, camera.forward, 20f, Physics.AllLayers, QueryTriggerInteraction.Collide);
+        Vector3 hitPoint = camera.position + camera.forward * 20f;
         foreach (RaycastHit raycastHit in targetsHit)
         {
             //Debug.Log(raycastHit.transform.name);
@@ -168,53 +118,11 @@ public class EletricPistol : Weapon
                 Damage damage = new Damage(unchargedHitDamage, Damage.DamageType.Eletric, true, weaponControl.transform.position);
                 idamageable.TakeDamage(damage);
                 ArmadilloUIControl.Instance.StartHitMarker();
-                if (!raycastHit.collider.isTrigger) break;
+                hitPoint = raycastHit.point;
+                if (!raycastHit.collider.isTrigger)break;
             }
         }
-        OnOverheatCharge(unchargedOverheatCharge);
-        visualHandler.OnUnchargedFire(camera.position + camera.forward * 20f);
-    }
-    public void ChargedFire()
-    {
-        RaycastHit[] targetsHit;
-        Transform camera = ArmadilloPlayerController.Instance.cameraControl.mainCamera.transform;
-        targetsHit = Physics.RaycastAll(camera.position, camera.forward, 30f,Physics.AllLayers,QueryTriggerInteraction.Collide);
-        foreach (RaycastHit raycastHit in targetsHit)
-        {
-            //Debug.Log(raycastHit.transform.name);
-            if (raycastHit.transform.TryGetComponent(out IDamageable idamageable))
-            {
-                Damage damage = new Damage(chargedHitDamage, Damage.DamageType.Eletric, true, weaponControl.transform.position);
-                idamageable.TakeDamage(damage);
-                ArmadilloUIControl.Instance.StartHitMarker();
-                if (!raycastHit.collider.isTrigger) break;
-            }
-        }
-        OnOverheatCharge(chargedOverheatCharge);
-        FPCameraShake.StopShake(holdShakeStats);
-        holdShakeStats = null;
-        visualHandler.OnChargedFire(camera.position + camera.forward * 30f);
-    }
-
-    public Coroutine holdOrPressTimerRef;
-    private float holdOrPressTimer;
-    private ShakeStats holdShakeStats;
-    public IEnumerator HoldOrPressTimer_Coroutine()
-    {
-        holdOrPressTimer = 0;
-        while (holdOrPressTimer < chargeTime)
-        {
-            holdOrPressTimer += Time.deltaTime;
-            yield return null;
-        }
-        ArmadilloPlayerController.Instance.visualControl.ToggleEletricPistolCharge(true);
-        visualHandler.OnCharge();
-        holdShakeStats = FPCameraShake.StartShake(0.05f, 1f);
-        while (true)
-        {
-            holdOrPressTimer += Time.deltaTime;
-            yield return null;
-        }
+        visualHandler.OnUnchargedFire(hitPoint);
     }
 
     #endregion
@@ -243,116 +151,11 @@ public class EletricPistol : Weapon
     }
     #endregion
 
-    //OverHeat
-    #region
-
-    public void UpdateOverheatHUD()
-    {
-        weaponControl.currentWeaponUI.text = " x" + currentAmmoAmount.ToString() + "%";
-        float currentPercentage = ((float)currentAmmoAmount) / ((float)maxAmmoAmount);
-        if (currentPercentage > 1) currentPercentage = 1;
-        ammoSlider.value = currentPercentage;
-        weaponControl.currentWeaponUI.color = new Color(1, 1 - currentPercentage, 1 - currentPercentage);
-        AmmoImage = ammoSlider.GetComponentInChildren<Image>();
-        AmmoImage.color = new Color(1, 1 - currentPercentage, 1 - currentPercentage);
-
-    }
-    public void OnOverheatCharge(int chargeAmount)
-    {
-        currentAmmoAmount += chargeAmount;
-        StopPassiveCoolOff();
-        if (currentAmmoAmount >= maxAmmoAmount) OnOverheat();
-        else
-        {
-            StartPassiveCoolOff();
-        }
-        UpdateOverheatHUD();
-    }
-    public void OnOverheat()
-    {
-        isOverheating = true;
-        visualHandler.OnEnterOverheatMode();
-        if (overheatTimer_Ref == null)
-        {
-            overheatTimer_Ref = weaponControl.StartCoroutine(OverheatTimer_Coroutine());
-        }
-        else
-        {
-            weaponControl.StopCoroutine(overheatTimer_Ref);
-            overheatTimer_Ref = weaponControl.StartCoroutine(OverheatTimer_Coroutine());
-        }
-        //start timer 
-    }
-
-    private Coroutine overheatTimer_Ref;
-    public IEnumerator OverheatTimer_Coroutine()
-    {
-        ArmadilloPlayerController.Instance.visualControl.ToggleEletricPistolOverheat(true);
-        yield return new WaitForSeconds(0.25f);
-        float timer = 0;
-        int startingAmmoAmount = currentAmmoAmount;
-        while (timer < overheatDuration)
-        {
-            currentAmmoAmount = Mathf.RoundToInt(Mathf.Lerp(startingAmmoAmount, 0, timer / (overheatDuration - 0.25f)));
-            UpdateOverheatHUD();
-            timer += 0.05f;
-            yield return new WaitForSeconds(0.05f);
-        }
-        isOverheating = false;
-        ArmadilloPlayerController.Instance.visualControl.ToggleEletricPistolOverheat(false);
-        visualHandler.OnLeaveOverheatMode();
-        currentAmmoAmount = 0;
-        UpdateOverheatHUD();
-    }
-    private void StartPassiveCoolOff()
-    {
-        if (passiveCoolOff_Ref == null) passiveCoolOff_Ref = weaponControl.StartCoroutine(PassiveCoolOff_Coroutine());
-    }
-    private void StopPassiveCoolOff()
-    {
-        if (passiveCoolOff_Ref != null)
-        {
-            weaponControl.StopCoroutine(passiveCoolOff_Ref);
-            passiveCoolOff_Ref = null;
-        }
-    }
-    private Coroutine passiveCoolOff_Ref;
-    public IEnumerator PassiveCoolOff_Coroutine()
-    {
-        yield return new WaitForSeconds(timeUntilPassiveCoolOffStarts);
-
-        float timer = 0;
-        float ammoAmountAsFloat = currentAmmoAmount;
-        while (ammoAmountAsFloat > 0)
-        {
-            currentAmmoAmount = Mathf.RoundToInt(ammoAmountAsFloat);
-            UpdateOverheatHUD();
-            timer += 0.05f;
-            yield return new WaitForSeconds(0.05f);
-            ammoAmountAsFloat -= (maxAmmoAmount / timeToFullCoolOff) * 0.05f;
-        }
-        currentAmmoAmount = 0;
-        UpdateOverheatHUD();
-        passiveCoolOff_Ref = null;
-    }
-    #endregion
-
     //Reset
     #region
     public override void ResetGun()
     {
         visualHandler.ResetVisuals();
-        if (holdOrPressTimerRef != null)
-        {
-            weaponControl.StopCoroutine(holdOrPressTimerRef);
-            holdOrPressTimerRef = null;
-        }
-        holdOrPressTimer = 0;
-        if (holdShakeStats != null)
-        {
-            FPCameraShake.StopShake(holdShakeStats);
-            holdShakeStats = null;
-        }
         isOnCooldown = false;
         fireCooldown_Ref = null;
         ArmadilloPlayerController.Instance.visualControl.ToggleEletricPistolCharge(false);
@@ -363,7 +166,12 @@ public class EletricPistol : Weapon
     #region Run
     public override void ToggleOnRun(bool state)
     {
-       
+
     }
+    #endregion
+
+    //Old Pistol
+    #region OldPistol
+
     #endregion
 }
--- Assets/Scripts/Weapons/EletricPistol/EletricPistolVisual.cs
+++ Assets/Scripts/Weapons/EletricPistol/EletricPistolVisual.cs
@@ -1,6 +1,4 @@
 using System.Collections;
-using System.Collections.Generic;
-using Unity.VisualScripting;
 using UnityEngine;
 
 public class EletricPistolVisual : MonoBehaviour
@@ -9,21 +7,27 @@ public class EletricPistolVisual : MonoBehaviour
 
     [SerializeField] private GameObject model;
     [Header("VFX")]
-    [SerializeField] private ParticleSystem chargedParticle;
     [SerializeField] private ParticleSystem onFireParticle;
     [SerializeField] private ParticleSystem onFireTrail;
-    [SerializeField] private ParticleSystem onOverheat;
-    [SerializeField] private ParticleSystem BzzOnomatopeiaParticle;
+    [SerializeField] private ParticleSystem onReloadParticle;
     [SerializeField] private ParticleSystem ZiumOnomatopeiaParticle;
+    [SerializeField] private LineRenderer projectileLineRenderer;
     [Header("Audio")]
     [SerializeField] public Transform audioEmitterTransform;
     [SerializeField] private SoundEmitter onUnchargedFireSoundEmitter;
-    [SerializeField] private SoundEmitter onChargedFireSoundEmitter;
-    [SerializeField] private SoundEmitter onChargingFireSoundEmitter;
+
+
+    float projectileLineRendererStartAlphaValue;
+    float projectileLineRendererEndAlphaValue;
 
     private void Awake()
     {
         Instance = this;
+    }
+    private void Start()
+    {
+        projectileLineRendererStartAlphaValue = projectileLineRenderer.startColor.a;
+        projectileLineRendererEndAlphaValue = projectileLineRenderer.endColor.a;
     }
     public void ToggleVisual(bool state)
     {
@@ -35,48 +39,41 @@ public class EletricPistolVisual : MonoBehaviour
 
         onFireParticle.Play();
 
-        onFireTrail.Play();
-        //TweenMunicao.Instance.ShakeAmmunition();
+        //onFireTrail.Play();
+        StartProjectileLineRenderer(destination);
+
         onUnchargedFireSoundEmitter.PlayAudio();
-        FPCameraShake.Instance.StartPivotOffsetFixed(0f, Vector3.back/2, 0.05f, 0.05f);
+        FPCameraShake.Instance.StartPivotOffsetFixed(0f, Vector3.back / 2, 0.05f, 0.05f);
     }
-    public void OnChargedFire(Vector3 destination)
-    {
-        OnUnCharge();
 
-        ZiumOnomatopeiaParticle.Play();
+    private void StartProjectileLineRenderer(Vector3 destination)
+    {
+        if (projectileLineUpdate_Ref != null) StopCoroutine(projectileLineUpdate_Ref);
+        projectileLineUpdate_Ref = StartCoroutine(ProjectileLineUpdate_Coroutine(destination));
+    }
 
-        onFireParticle.Play();
+    private Coroutine projectileLineUpdate_Ref;
+    private IEnumerator ProjectileLineUpdate_Coroutine(Vector3 destination)
+    {
+        float timer = 0f;
+        float duration = 0.2f;
 
-        onFireTrail.Play();
-        //TweenMunicao.Instance.ShakeAmmunition();
-        onChargedFireSoundEmitter.PlayAudio();
-        FPCameraShake.Instance.StartPivotOffsetFixed(0f, Vector3.back, 0.1f, 0.1f);
+        projectileLineRenderer.enabled = true;
+        while (timer < duration)
+        {
+            projectileLineRenderer.startColor = new Color(projectileLineRenderer.startColor.r, projectileLineRenderer.startColor.g, projectileLineRenderer.startColor.b, projectileLineRendererStartAlphaValue * (1 - timer / duration));
+            projectileLineRenderer.endColor = new Color(projectileLineRenderer.endColor.r, projectileLineRenderer.endColor.g, projectileLineRenderer.endColor.b, projectileLineRendererEndAlphaValue * (1 - timer / duration));
+            projectileLineRenderer.SetPosition(1, transform.InverseTransformPoint(destination));
+            timer += Time.deltaTime;
+            yield return null;
+        }
+        projectileLineRenderer.enabled = false;
+        projectileLineRenderer.startColor = new Color(projectileLineRenderer.startColor.r, projectileLineRenderer.startColor.g, projectileLineRenderer.startColor.b, projectileLineRendererStartAlphaValue);
+        projectileLineRenderer.endColor = new Color(projectileLineRenderer.endColor.r, projectileLineRenderer.endColor.g, projectileLineRenderer.endColor.b, projectileLineRendererEndAlphaValue);
     }
-    public void OnCharge()
-    {
-        onChargingFireSoundEmitter.PlayAudio();
-        chargedParticle.Play();
-        BzzOnomatopeiaParticle.Play();
-    }
-    public void OnUnCharge()
-    {
-        onChargingFireSoundEmitter.StopAudio();
-        chargedParticle.Stop();
-        BzzOnomatopeiaParticle.Stop();
-    }
-    public void OnEnterOverheatMode()
-    {
-        onOverheat.Play();
-    }
-    public void OnLeaveOverheatMode()
-    {
-        onOverheat.Stop();
-    }
+
     public void ResetVisuals()
     {
-        onChargingFireSoundEmitter.StopAudio();
-        chargedParticle.Stop();
-        BzzOnomatopeiaParticle.Stop();
+
     }
 }
