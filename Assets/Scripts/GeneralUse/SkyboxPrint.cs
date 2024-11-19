78e502445679cb44ee5abdb31de017aab2557588
Mori <contato.devmori@gmail.com>
2024-11-19 10:49:04 -0300

    SkyboxPrint

--- Assets/Scripts/GeneralUse/SkyboxPrint.cs
+++ Assets/Scripts/GeneralUse/SkyboxPrint.cs
@@ -1,17 +1,23 @@
+#if UNITY_EDITOR
+using UnityEditor;
+#endif
 using UnityEngine;
 
 public class SkyboxPrint : MonoBehaviour
 {
     [SerializeField]bool m_print;
-    [SerializeField] int resolution;
-    private void OnValidate()
+    [SerializeField] private Cubemap cubeMapRef;
+
+    private void Update()
     {
 #if UNITY_EDITOR
         if (m_print)
         {
-            ScreenCapture.CaptureScreenshot("Assets\\Screenshots\\" + "PrintSkybox.png", resolution);
+            GetComponent<Camera>().RenderToCubemap(cubeMapRef);
+            //AssetDatabase.CreateAsset(cubeMap, "Assets\\Screenshots\\SkyboxPrint.asset");
             m_print = false;
         }
     }
 #endif
 }
+
