8f8409329cb6a5a77fd35ccf287261d3b1752e5e
Rodrigo Morizono <rkmorizono@gmail.com>
2024-11-29 23:51:51 -0300

    BossFight Music

--- Assets/Scripts/UI/OnDemoFinish.cs
+++ Assets/Scripts/UI/OnDemoFinish.cs
@@ -17,15 +17,21 @@ public class OnDemoFinish : MonoBehaviour
     }
     private IEnumerator EnterEndScreen_Coroutine()
     {
+        ArmadilloPlayerController.Instance.inputControl.TogglePlayerControls(false);
+        ArmadilloPlayerController.Instance.inputControl.TogglePauseControls(false);
         float timer =0f;
-        float duration = 1f;
+        float duration = 3f;
 
         while(timer < duration)
         {
+            Time.timeScale = timer / duration;
             canvasGroup.alpha = timer/duration;
-            timer += Time.deltaTime;
+            timer += Time.unscaledDeltaTime;
             yield return null;
         }
+        Cursor.visible = true;
+        Cursor.lockState = CursorLockMode.None;
+        Time.timeScale = 0;
         canvasGroup.alpha = 1;
         canvasGroup.interactable = true;
         canvasGroup.blocksRaycasts = true;
